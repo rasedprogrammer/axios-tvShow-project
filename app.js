b250ab3e6c263d12c18ddd7a22e83e2d85427cac
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-09 00:38:27 +0600

    complete full project just in time

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const form = document.querySelector("#searchForm");
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTrem = form.elements.query.value;
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${searchTrem}`
-  );
+  const config = { params: { q: searchTrem } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  makeImages(res.data);
+  form.elements.query.value = "";
 });
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
